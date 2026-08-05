68ff86443de48d970ac7b163a334bd7f2293fd85
TheBlueGuy7 <bartalis.hunor17@gmail.com>
2026-08-05 15:40:21 +0200

    fastfetch

--- flake.nix
+++ flake.nix
@@ -52,8 +52,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    unsloth-llama-src = {
-      url = "github:unslothai/llama.cpp"; 
+    unsloth-tarball-src = {
+      url = "https://github.com/unslothai/llama.cpp/releases/download/b10225-mix-345e1e3/llama.cpp-source-commit-d2a74a4a3cd1bbc60127bebc6f8334e987d6d005.tar.gz";
       flake = false;
     };
 
@@ -69,7 +69,7 @@
     hyprland,
     noctalia,
     stylix,
-    unsloth-llama-src,
+    unsloth-tarball-src,
     ...
   } @ inputs: let
     system = "x86_64-linux";
@@ -88,24 +88,45 @@
         # inputs.disko.nixosModules.default
         inputs.agenix.nixosModules.default
         inputs.stylix.nixosModules.stylix
-	({ pkgs, ... }: {
-          nixpkgs.config.allowUnfree = true;
-
+	({ pkgs, inputs, ... }: {
           nixpkgs.overlays = [
-            (final: prev: {              
-              llama-cpp = (prev.llama-cpp.override { 
-                cudaSupport = true; 
+            (final: prev: {
+              llama-cpp = (prev.llama-cpp.override {
+                cudaSupport = true;
+                cudaPackages = final.cudaPackages; # Uses your system's CUDA
               }).overrideAttrs (oldAttrs: {
                 pname = "llama-cpp-unsloth";
                 version = "10225";
-                src = unsloth-llama-src;
-                patches = [];
-		npmDepsHash = "sha256-pjdbI6NcZRlJVd62xhgbLhWrwFYwgsIwjORqvo1+VD8=";
-              });
+                
+                # Use the official Unsloth release tarball
+                src = inputs.unsloth-tarball-src;
+                
+                # Keep NixOS patches so the binary installs correctly
+                patches = oldAttrs.patches or [];
 
+		npmDepsHash = "sha256-B7uEynAG70a3xauBKc20RuFa9cnWaWzVBCh+LPLBnIM=";
+                
+                cmakeFlags = (oldAttrs.cmakeFlags or []) ++ [
+                  (prev.lib.cmakeBool "GGML_NATIVE" false)
+                  (prev.lib.cmakeBool "GGML_CPU_ALL_VARIANTS" false)
+                  
+                  # GTX 1650 is Turing architecture (Compute Capability 7.5)
+                  # This tells the compiler to ONLY build for your specific GPU.
+                  (prev.lib.cmakeFeature "CMAKE_CUDA_ARCHITECTURES" "75")
+                ];
+
+                env = (oldAttrs.env or {}) // {
+                  # i5-10400F is Comet Lake, which uses Skylake lineage instructions
+                  NIX_CFLAGS_COMPILE = toString ((oldAttrs.env.NIX_CFLAGS_COMPILE or "") + " -march=skylake -mtune=skylake");
+                };
+
+                preConfigure = (oldAttrs.preConfigure or "") + ''
+                  export NIX_ENFORCE_NO_NATIVE=0
+                '';
+              });
             })
           ];
-        })
+        })	
       ] ++ modules;
     };
 
