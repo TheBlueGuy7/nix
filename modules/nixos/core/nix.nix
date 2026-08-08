36512112816e5f065fbd5aa7ef4fb40a9df18ad0
TheBlueGuy7 <bartalis.hunor17@gmail.com>
2026-08-08 13:02:14 +0200

    updated configs

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -3,9 +3,11 @@
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     auto-optimise-store = true;
-    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-substituters = [ "https://noctalia.cachix.org" "https://cache.nixos.org" "https://nix-community.cachix.org" ];
     extra-trusted-public-keys = [
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
   nix.optimise.automatic = true;
